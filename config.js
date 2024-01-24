8e5d627f3a4968dedb9154e28b38133de2a383f8
manolo2829 <manudiez2003@gmail.com>
2024-01-24 17:49:54 -0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const TOKEN_SECRET = 'asdfe45we45w345wegw345werjktjwertkj'
-export const MONGO_URI = process.env.MONGO_URI
+export const MONGO_URI = process.env.DEV === 'DEV' ? "mongodb://localhost:27017/finazapp" : process.env.MONGO_URI
 export const PORT = process.env.PORT || 8080
-export const ORIGIN_URI = process.env.ORIGIN_URI 
+export const ORIGIN_URI = process.env.DEV === 'DEV' ? "http://localhost:3000" : process.env.ORIGIN_URI
